cebebc9eeaa3d9952e4d325b405f2ad6295e4387
Breno Romeiro <bsromeiro@gmail.com>
2022-12-22 17:00:01 -0300

    add: graph and search header

--- src/components/Main/Chart.tsx
+++ src/components/Main/Chart.tsx
@@ -1,1 +1,94 @@
-export const Chart = () => <article className="h-[60%] bg-section"></article>;
+import { ResponsiveLine } from "@nivo/line";
+export const Chart = () => (
+  <article className="h-[60%]">
+    <ResponsiveLine
+      data={[
+        {
+          id: "positive :)",
+          data: [
+            { x: 0, y: 0.7 },
+            { x: 1, y: 0.9 },
+            { x: 2, y: 0.8 },
+            { x: 3, y: 0.6 },
+            { x: 4, y: 0.3 },
+            { x: 5, y: 0 },
+            { x: 6, y: null },
+            { x: 7, y: null },
+            { x: 8, y: null },
+            { x: 9, y: null },
+            { x: 10, y: null },
+            { x: 11, y: 0 },
+            { x: 12, y: 0.4 },
+            { x: 13, y: 0.6 },
+            { x: 14, y: 0.5 },
+            { x: 15, y: 0.3 },
+            { x: 16, y: 0.4 },
+            { x: 17, y: 0 },
+          ],
+        },
+        {
+          id: "negative :(",
+          data: [
+            { x: 5, y: 0 },
+            { x: 6, y: -0.3 },
+            { x: 7, y: -0.5 },
+            { x: 8, y: -0.9 },
+            { x: 9, y: -0.2 },
+            { x: 10, y: -0.4 },
+            { x: 11, y: 0 },
+            { x: 12, y: null },
+            { x: 13, y: null },
+            { x: 14, y: null },
+            { x: 15, y: null },
+            { x: 16, y: null },
+            { x: 17, y: 0 },
+            { x: 18, y: -0.4 },
+            { x: 19, y: -0.2 },
+            { x: 20, y: -0.1 },
+            { x: 21, y: -0.6 },
+          ],
+        },
+      ]}
+      // curve={select('curve', curveOptions, 'monotoneX')}
+      enablePointLabel={true}
+      // pointSymbol={CustomSymbol}
+      pointSize={14}
+      pointBorderWidth={1}
+      pointBorderColor={{
+        from: "color",
+        modifiers: [["darker", 0.3]],
+      }}
+      pointLabelYOffset={-20}
+      enableGridX={false}
+      theme={{
+        textColor: "white",
+        tooltip: {
+          basic: { color: "black" },
+        },
+      }}
+      colors={["rgb(97, 205, 187)", "rgb(244, 117, 96)"]}
+      xScale={{
+        type: "point",
+      }}
+      yScale={{
+        type: "linear",
+        min: -1,
+        max: 1,
+      }}
+      enableArea={true}
+      areaOpacity={0.07}
+      enableSlices={false}
+      useMesh={true}
+      crosshairType="cross"
+      margin={{ left: 50, right: 20, bottom: 35, top: 35 }}
+      axisBottom={{
+        tickSize: 5,
+        tickPadding: 2,
+        tickRotation: 0,
+        legend: "days",
+        legendOffset: 13,
+        legendPosition: "end",
+      }}
+    />
+  </article>
+);
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,23 +1,34 @@
-import { FaBeer } from "react-icons/fa";
-import UpgradeBg from "../../assets/images/upgrade-bg.png";
-
-import { BsThreeDotsVertical } from "react-icons/bs";
+import { BsSliders, BsThreeDotsVertical } from "react-icons/bs";
+import { FiSearch } from "react-icons/fi";
 import { VscBellDot } from "react-icons/vsc";
-import { Upgrade } from "../Upgrapde";
 import { Totals } from "../Totals";
-import { Chart } from "./Chart";
+import { Upgrade } from "../Upgrapde";
 import { Balance } from "./Balance";
 import { Buy } from "./Buy";
+import { Chart } from "./Chart";
 
 export const Main = () => {
   return (
     <section className="w-full">
-      <header className="h-[12vh] flex justify-between p-10 relative">
+      <header className="h-[12vh] flex justify-between p-5 relative items-center">
         <h1 className="text-2xl">
           Welcome back, <b>Sally</b>
         </h1>
 
-        <nav className="flex items-center gap-4">
+        <nav className="flex items-center gap-4 h-full">
+          <div className="bg-section h-14 w-[500px] rounded-lg flex items-center justify-between px-4 gap-4">
+            <div className="w-full flex items-center gap-4">
+              <FiSearch size={28} />
+              <input
+                type="text"
+                className="w-full bg-transparent"
+                placeholder="Search"
+              />
+            </div>
+            <button>
+              <BsSliders size={24} />
+            </button>
+          </div>
           <VscBellDot size={24} />
           <div className="w-8 h-8 rounded-full bg-purple ml-2"></div>
           <BsThreeDotsVertical size={24} />
@@ -29,9 +40,7 @@ export const Main = () => {
       <main className="h-[88vh] w-full flex px-5 pb-5 [&>section]:flex [&>section]:flex-col [&>section]:gap-6 gap-6">
         <section className="w-[70%] [&>article]:w-full  [&>article]:rounded-xl">
           <Upgrade />
-
           <Totals />
-
           <Chart />
         </section>
 
